83105b17ddb447852505ac214a0abf008427df99
KosarAnsari <kosaransari55@gmail.com>
2025-07-29 22:49:19 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,43 @@
+import Counter from './Counter.jsx';
+import Toggle from './Toggle.jsx';
+import Input from './Input.jsx';
+import Color from './Color.jsx';
+import Light from './Light.jsx';
+import Daytime from './Daytime.jsx';
+import Log from './Log.jsx';
+import Form from './Form.jsx';
+import Image from './Image.jsx';
+import Todo from './Todo.jsx';
+import Charcount from './Charcount';
+import Like from './Like';
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <hr />
+      <Toggle />
+      <hr />
+      <Input />
+      <hr />
+      <Color />
+      <hr />
+      <Light />
+      <hr />
+      <Daytime />
+      <hr />
+      <Log />
+      <hr />
+      <Form />
+      <hr />
+      <Image />
+      <hr />
+      <Todo />
+      <hr />
+      <Charcount />
+      <hr />
+      <Like />
+    </>
+  );
+}
+export default App;
